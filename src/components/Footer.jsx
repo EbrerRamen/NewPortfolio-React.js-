6d821534be3a468fed1210f8e0d52b2019eac456
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-07 01:50:35 +0600

    Matched footer color with main layout color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="py-6 bg-indigo-600 text-center text-white text-sm">
+        <footer className="py-6 bg-gray-900/30 border-t border-gray-700/40 text-center text-gray-300 text-sm backdrop-blur-md">
             © {new Date().getFullYear()} Md. Abrar Rahman Shafin. All rights reserved.
         </footer>
     );
